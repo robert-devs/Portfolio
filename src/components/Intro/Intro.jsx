972f33a65cc1e2772fbbee83763e538bc543aab8
robert-devs <mainrobert04@gmail.com>
2023-01-31 19:46:32 +0300

    adding the intro component

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
-import "./Intro.scss"
+import './Intro.scss'
+import { init } from 'ityped'
+import { ArrowDownward } from '@material-ui/icons'
+import { useEffect } from 'react'
+import { useRef } from 'react'
+// import ArrowDownwardIcon from '@mui/icons-material/Ar';/
 
 export default function Intro() {
+  const textRef = useRef()
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backSpeed:  50,
+      backDelay:  1500,
+      strings: ['Developer', 'Designer'],
+    })
+  }, [])
   return (
-    <div className='intro' id="intro">
-      
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/man.avif" alt="pic" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2> Hi There, i'm</h2>
+          <h1> Robert Maina</h1>
+          <h3>
+            Freelancer <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          {/* <img src={ArrowDown} alt="" /> */}
+          <ArrowDownward
+            style={{ color: 'rgb(255, 5, 88)', alignItems: 'center' }}
+            className="img"
+          />
+        </a>
+      </div>
     </div>
   )
 }
