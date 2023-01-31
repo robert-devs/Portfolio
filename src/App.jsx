bd5690c267460b11f2a36ed79c45fd952fb9ba5f
robert-devs <mainrobert04@gmail.com>
2023-01-31 16:05:53 +0300

    adding toggle menu

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
      <Navbar menuOpen= {menuOpen} setMenuOpen = {setMenuOpen} />
-     <Menu openMenu = {menuOpen} setMenuOpen={setMenuOpen} />
+     <Menu menuOpen= {menuOpen} setMenuOpen = {setMenuOpen} />
    
      <div className="sections">
       
