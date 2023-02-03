08cecc4b4f9357dc969700cbdb1f645c36f3600c
robert-devs <mainrobert04@gmail.com>
2023-02-03 14:27:17 +0300

    adding  the responsiveness

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,21 +1,34 @@
+import { Spa } from '@material-ui/icons'
 import React from 'react'
-import "./Contact.scss"
+import { useState } from 'react'
+import './Contact.scss'
 
 export default function Contact() {
+  const [message, setMessage] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setMessage(true)
+  }
   return (
-    <div className='contact' id='contact'>
+    <div className="contact" id="contact">
       <div className="left">
-        <img src="assets.jpg" alt="" />
+        <img src="assets/shake.avif" alt="" />
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form >
-          <input type="text" placeholder='Email' />
-          <textarea name="" id="" cols="30" rows="10" placeholder='message'></textarea>
-          <button type='submit'>Send</button>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="message"
+          ></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I ill reply ASAP:</span>}
         </form>
       </div>
     </div>
   )
 }
-
