abbd4271f1ca1db2424daa9fcbdbce980cf6099e
robert-devs <mainrobert04@gmail.com>
2023-02-15 15:23:56 +0300

    updating  the logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,19 +4,19 @@ import { Person, Mail } from '@material-ui/icons'
 
 export default function Navbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className={"nav " + (menuOpen && "active")}>
+    <div className={'nav ' + (menuOpen && 'active')}>
       <div className="wrapper">
         <div className="left">
           <a href="intro" className="logo">
-            Genius.
+            <img src="assets/robert3.png" alt="" />
           </a>
           <div className="itemContainer">
             <Person className="icon" />
-            <span>+254 8930</span>
+            <span>+254 070 081 2847 </span>
           </div>
           <div className="itemContainer">
             <Mail className="icon" />
-            <span>sdk maina@gmail.com</span>
+            <span> mainarobert04@gmail.com</span>
           </div>
         </div>
         <div className="right">
